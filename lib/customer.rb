c2188e53db287b29c6728374399a0dd3f4386c56
Jonathan Norman <jnorman2123@gmail.com>
2019-10-23 13:29:52 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,7 +2,7 @@ class Customer
   attr_accessor :name, :age
   @@all_customers = []
 
-  def initialize(name)
+  def initialize(name, age)
     @name = name
     @age = age
     @@all_customers << self
